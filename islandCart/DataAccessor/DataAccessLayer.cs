0d4fa39f7b5b9849be9ba3aee4973c58badde1b0
Ivan Begue <evansbegue@gmail.com>
2024-06-07 08:50:42 +0400

    Data Accessor added using 3-tier architecture

---
+++ islandCart/DataAccessor/DataAccessLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace islandCart.DataAccessor
+{
+    public class DataAccessLayer
+    {
+        private readonly string _conString;
+
+        public DataAccessLayer()
+        {
+            _conString = WebConfigurationManager.ConnectionStrings["islandCartDB"].ConnectionString; 
+        }
+
+        public SqlDataReader GetQuery(string query, out SqlConnection con)
+        {
+
+            con = new SqlConnection(_conString);
+            SqlCommand cmd = new SqlCommand
+            {
+                Connection = con,
+                CommandType = CommandType.Text,
+                CommandText = query
+            };
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+
+        }
+    }
+}
